2a49aa14a08b6d73be16a85e3e6b40aac77cc000
gckeller <g.k.keller@uu.nl>
2023-06-28 14:25:51 +0200

    Update Plugin.hs

--- accelerate-llvm-native/src/Data/Array/Accelerate/LLVM/Native/Plugin.hs
+++ accelerate-llvm-native/src/Data/Array/Accelerate/LLVM/Native/Plugin.hs
@@ -35,6 +35,7 @@ import Linker
 import SysTools
 #endif
 
+import GHC (hasTarget (..))
 
 -- | This GHC plugin is required to support ahead-of-time compilation for the
 -- accelerate-llvm-native backend. In particular, it tells GHC about the
